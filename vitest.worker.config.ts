87d851f3f0b3849148298321097193ca043eeb44
cloudcorenow <138939871+cloudcorenow@users.noreply.github.com>
2026-01-11 20:46:45 -0800

    Updated vitest.worker.config.ts

---
+++ vitest.worker.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/confi';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'miniflare',
+    environmentOptions: {
+      bindings: {
+        DB: 'TEST_DB',
+        KV: 'TEST_KV'
+      },
+      kvNamespaces: ['TEST_KV'],
+      d1Databases: ['TEST_DB']
+    },
+    setupFiles: ['./src/test/worker-setup.ts'],
+  },
+});
